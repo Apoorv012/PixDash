11602bae7d80d4d4d9e8448d6c2ddbafff351be0
Apoorv Mittal <apoorvmitthu@gmail.com>
2025-12-28 22:08:09 +0530

    Added animations, and smooth movements

--- Assets/_Project/Scripts/Player/Player.cs
+++ Assets/_Project/Scripts/Player/Player.cs
@@ -1,56 +1,54 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 10f;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
-    public float groundRadius = 0.2f;
+	[SerializeField] private float moveSpeed = 5f;
+	[SerializeField] private float jumpPower = 10f;
+	[SerializeField] private Rigidbody2D rb;
+	[SerializeField] private Animator animator;
 
-    private Rigidbody2D rb;
-    private float horizontal;
-    private bool isGrounded;
+	float horizontalInput;
+	bool isFacingRight = true;
+	bool isGrounded = false;
 
-    private bool FacingRight = true;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void Update()
-    {
-        horizontal = Keyboard.current.aKey.isPressed ? -1 :
-                     Keyboard.current.dKey.isPressed ? 1 : 0;
-
-        if((horizontal==-1&&FacingRight==true)||(horizontal==1&&FacingRight==false))
-        {
-            Flip();
-        }
-        isGrounded = Physics2D.OverlapCircle(
-            groundCheck.position,
-            groundRadius,
-            groundLayer
-        );
-        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        rb.linearVelocity = new Vector2(horizontal * moveSpeed, rb.linearVelocity.y);
-    }
-
-    	private void Flip()
+	// Update is called once per frame
+	void Update()
 	{
-		FacingRight = !FacingRight;
+		horizontalInput = Input.GetAxis("Horizontal");
 
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
+		FlipSprite();
+
+		if (Input.GetButtonDown("Jump") && isGrounded)
+		{
+			rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+			isGrounded = false;
+			animator.SetBool("isJumping", !isGrounded);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+		animator.SetFloat("xVelocity", Mathf.Abs(rb.linearVelocity.x));
+		animator.SetFloat("yVelocity", rb.linearVelocity.y);
+	}
+
+	void FlipSprite()
+	{
+		if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+		{
+			isFacingRight = !isFacingRight;
+			Vector3 ls = transform.localScale;
+			ls.x *= -1f;
+			transform.localScale = ls;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		isGrounded = true;
+		animator.SetBool("isJumping", !isGrounded);
 	}
 }
